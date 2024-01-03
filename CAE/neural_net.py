8c6efe50cf0fbd2dd5bf32a533ac735c2cfec7dc
Yi Yang <yy4022@ic.ac.uk>
2024-01-03 20:10:29 +0800

    implement: complete the Decoder structure

--- CAE/neural_net.py
+++ CAE/neural_net.py
@@ -61,3 +61,66 @@ class Encoder(nn.Module):
         x = self.flatten(x)
         x = self.encoder_lin(x)
         return x
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, img_scale):
+        super().__init__()
+
+        self.img_scale = img_scale
+        self.current_size = ceil((((((img_scale - 5) / 4 + 1) - 3) / 2 + 1) - 2) / 2 + 1)
+
+        # Linear section
+        self.decoder_lin = nn.Sequential(
+            # 1st fully connected layer
+            nn.Linear(in_features=100, out_features=128),
+            nn.LeakyReLU(negative_slope=0.3),
+
+            # 2nd fully connected layer
+            nn.Linear(in_features=128, out_features=32 * self.current_size),
+            nn.LeakyReLU(negative_slope=0.3),
+
+            # 3rd fully connected layer
+            nn.Linear(in_features=32 * self.current_size,
+                      out_features=32 * self.current_size * self.current_size),
+            nn.LeakyReLU(negative_slope=0.3),
+        )
+
+        # Unflatten section
+        self.unflatten = nn.Unflatten(dim=1,
+                                      unflattened_size=(32, self.current_size, self.current_size))
+
+        # Convolutional section
+        self.decoder_conv = nn.Sequential(
+            # 1st Convolutional layer
+            nn.Conv2d(in_channels=32, out_channels=128, kernel_size=2, stride=1, padding=1),
+            nn.LeakyReLU(negative_slope=0.3),
+            # 1st Upsampling layer
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+
+            # 2nd Convolutional layer
+            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(negative_slope=0.3),
+            # 2nd Upsampling layer
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+
+            # 3rd Convolutional layer
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(negative_slope=0.3),
+            # 3rd Upsampling layer
+            nn.Upsample(scale_factor=4, mode='bilinear'),
+
+            # 4th Convolutional layer
+            nn.Conv2d(in_channels=32, out_channels=1, kernel_size=3, stride=1, padding=1),
+        )
+
+    # implement the decoder network in the given image
+    def forward(self, x):
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        # crop the image size
+        cropper = transforms.CenterCrop(size=(self.img_scale, self.img_scale))
+        x = cropper(x)
+        return x
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import torch
 from torchsummary import summary
 
-from CAE.neural_net import Encoder
+from CAE.neural_net import Encoder, Decoder
 
 # PART 1: define the parameters
 # 1.1. choose the device
@@ -15,10 +15,19 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 print(f"Selected device: {device}")
 
 # PART 2: preparation before training the model
-# 1. define the FullyCNN model
+# 1. define the Encoder model
 img_scale = 373
-fullyCNN = Encoder(img_scale)
+encoder = Encoder(img_scale)
 
-fullyCNN = fullyCNN.to(device)
+encoder = encoder.to(device)
 input_shape = (1, img_scale, img_scale)
-summary(fullyCNN, input_shape)
+summary(encoder, input_shape)
+
+# 2. define the Decoder model
+img_scale = 373
+decoder = Decoder(img_scale)
+
+decoder = decoder.to(device)
+input_shape = (100,)
+summary(decoder, input_shape)
+
